4c9fd4425e69a2944d38a14489b319041f2a6d8b
suprabathk <suprabathkondapally@gmail.com>
2022-11-20 16:11:14 +0530

    Changed port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const process = require("process");
 
-app.listen(3000, () => {
+app.listen(process.env.PORT | 3000, () => {
   console.log("Started express server at port 3000");
 });
